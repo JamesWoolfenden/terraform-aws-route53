f1c360b995024c86b87de73828d56e5daf989293
James Woolfenden <james.woolfenden@gmail.com>
2021-08-26 15:57:31 +0100

    refresh

---
+++ example/examplea/outputs.tf
@@ -0,0 +1,7 @@
+output "internal" {
+  value = module.route53.internal
+}
+
+output "primary" {
+  value = module.route53.primary
+}
---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "internal" {
+  value = aws_route53_zone.internal
+}
+
+output "primary" {
+  value = aws_route53_zone.primary
+}
